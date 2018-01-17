b22574fc61e8c2f7c945d270c07c600bda296ed5
Danny Baumann <dannybaumann@web.de>
2018-01-17 12:19:06 +0100

    Fix selection highlight in multi-pane mode.
    Signed-off-by: Danny Baumann <dannybaumann@web.de>

--- mobile/src/main/java/org/openhab/habdroid/ui/OpenHABWidgetListFragment.java
+++ mobile/src/main/java/org/openhab/habdroid/ui/OpenHABWidgetListFragment.java
@@ -142,8 +142,17 @@ public class OpenHABWidgetListFragment extends Fragment
                 openHABBaseUrl, openHABUsername, openHABPassword, this,
                 getResources().getInteger(R.integer.pager_columns) > 1);
 
+        if (savedInstanceState != null) {
+            openHABWidgetAdapter.setSelectedPosition(savedInstanceState.getInt("selection", -1));
+        }
         mRecyclerView.setLayoutManager(new LinearLayoutManager(mActivity));
         mRecyclerView.setAdapter(openHABWidgetAdapter);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("selection", openHABWidgetAdapter.getSelectedPosition());
     }
 
     @Override
